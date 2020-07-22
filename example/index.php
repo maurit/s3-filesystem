a7dd1c1cdbd4019d67ce86d921e56929ba723fa5
Adam Kabac <adam.kabac@maurit.sk>
2020-07-22 14:50:21 +0200

    comment example

--- example/index.php
+++ example/index.php
@@ -8,15 +8,15 @@ require_once '../vendor/autoload.php';
 require_once '../src/S3Storage.php';
 require_once '../src/Exceptions.php';
 
-$s3storage = new \Maurit\S3filesystem\S3Storage([
-    'region'         => 's3_config_region', // required
-    'endpoint'       => 's3_config_endpoint_url', // required
-    'key'            => 's3_public_key', // required
-    'secret'         => 's3_private_key', // required
-    'bucket'         => 's3_bucket_name', // required
-    'version'        => '2006-03-01', // optional default 2006-03-01
-    'cacheDirectory' => 'cache', // optional default cache in current dir
-    'cacheLifetime'  => 60 * 60, // optional default 60*60 (1 hour)
-]);
+//$s3storage = new \Maurit\S3filesystem\S3Storage([
+//    'region'         => 's3_config_region', // required
+//    'endpoint'       => 's3_config_endpoint_url', // required
+//    'key'            => 's3_public_key', // required
+//    'secret'         => 's3_private_key', // required
+//    'bucket'         => 's3_bucket_name', // required
+//    'version'        => '2006-03-01', // optional default 2006-03-01
+//    'cacheDirectory' => 'cache', // optional default cache in current dir
+//    'cacheLifetime'  => 60 * 60, // optional default 60*60 (1 hour)
+//]);
 
-$s3storage->render('toskansko/IMG_20190402_131847.jpg');
+//$s3storage->render('toskansko/IMG_20190402_131847.jpg');
