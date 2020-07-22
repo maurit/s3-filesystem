5034f3d9e2567379222473f99a9132efdf5eb3be
Adam Kabac <adam.kabac@maurit.sk>
2020-07-22 14:37:11 +0200

    example update

--- example/index.php
+++ example/index.php
@@ -14,10 +14,9 @@ $s3storage = new \Maurit\S3filesystem\S3Storage([
     'key'            => 's3_public_key', // required
     'secret'         => 's3_private_key', // required
     'bucket'         => 's3_bucket_name', // required
-    'version'        => 's3_config', // optional default 2006-03-01
-    'cacheDirectory' => 'path_to_save_cache', // optional default cache in current dir
-    'cacheDirectory' => 'path_to_save_cache', // optional default cache in current dir
-    'cacheLifetime'  => 'cace_lifetime_in_seconds', // optional default 60*60 (1 hour)
+    'version'        => '2006-03-01', // optional default 2006-03-01
+    'cacheDirectory' => 'cache', // optional default cache in current dir
+    'cacheLifetime'  => 60 * 60, // optional default 60*60 (1 hour)
 ]);
 
 $s3storage->render('toskansko/IMG_20190402_131847.jpg');
